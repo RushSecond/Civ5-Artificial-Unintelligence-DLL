8a7f143182dd605e7d58545017469742e341a8af
Blake Zoeckler <blake.rushhour@gmail.com>
2016-03-07 16:53:39 -0800

    Header updates
    More defines in RAI.h

--- CvGameCoreDLL_Expansion2/RAI.h
+++ CvGameCoreDLL_Expansion2/RAI.h
@@ -4,6 +4,8 @@
 #ifndef RAI_MODS_H
 #define RAI_MODS_H
 
+//** TACTICAL AI **//
+
 // AI will use safe bombard to attack enemy embarked units
 #define RAI_DESTROY_ENEMY_EMBARKED_UNITS
 
@@ -25,7 +27,32 @@
 // AI now properly fortifies units that didnt move this turn
 #define RAI_FORTIFY_IF_NO_MOVE
 
+// More AI logging information for CvTactialAI.cpp
 #define RAI_LOGGING_FIXES
+
+//** DIPLOMACY AI **//
+
+// AI weighs damage dealt to himself and damage dealt to opponent differently when considering peace deals
+#define RAI_DAMAGE_DEALT_PEACE_CHANGE
+
+// AI no longer takes into account player score when evaluating war score since it's a very inaccurate assessment
+#define RAI_WAR_SCORE_DOESNT_CONSIDER_PLAYER_SCORE
+
+// AI will now tend to use co-op war to gang up on stronger opponents, instead of crushing weaker ones
+#define RAI_COOP_WAR_USED_AGAINST_STRONGER_ENEMIES
+
+// AI will no longer have a chance to have REDUCED warmonger hate vs someone who has killed multiple players
+#define RAI_WARMONGER_PERCENT_PLAYERS_KILLED_FIX
+
+// AIs with low war approach will no longer have as much of a bonus to declaring war on weakened civs
+#define RAI_ATTACKING_WEAK_PLAYER_DEPENDS_ON_WAR_APPROACH
+
+// More accurate comparsion of players going for tourism victory
+#define RAI_GUESS_CULTURE_PRIORITY_COMPARES_TOURISM_TO_WORLD_CULTURE
+
+// Starting at King difficulty, the AI will now want to attack a player who is close to winning by Science, Culture, or Diplomacy
+// This effect gets harsher each successive difficulty level
+#define RAI_AI_TRIES_TO_STOP_WIN_CONDITIONS
 
 #endif
 
