342aefc0a997ed3a57648a61e0c67169bc5bb8cb
Blake Zoeckler <blake.rushhour@gmail.com>
2016-03-15 16:52:09 -0700

    Header Updated

--- CvGameCoreDLL_Expansion2/RAI.h
+++ CvGameCoreDLL_Expansion2/RAI.h
@@ -27,6 +27,14 @@
 // AI now properly fortifies units that didnt move this turn
 #define RAI_FORTIFY_IF_NO_MOVE
 
+// Fixed an AuI bug where siege units think they can reposition and then attack when they actually could not
+// Note to DelnarErsike: pNode->m_iData1 returns movement allowance remaining, TIMES 60.
+#define RAI_SEIGE_UNITS_REPOSITION_FIX
+
+// Default posture for enemy city zones is no longer surgical strike, instead it's withdraw.
+// This makes AI not attack city unitl they are READY.
+#define RAI_ONLY_ATTACK_CITY_WHEN_READY
+
 // More AI logging information for CvTactialAI.cpp
 #define RAI_LOGGING_FIXES
 #define PATH_FINDER_LOGGING
@@ -55,7 +63,8 @@
 // This effect gets harsher each successive difficulty level
 #define RAI_AI_TRIES_TO_STOP_WIN_CONDITIONS
 
-
+// Rich AI civs will much more aggressively try to outbid others on city states
+#define RAI_RICH_AI_OUTBIDS_OTHERS_FOR_CITY_STATES
 
 //** UNIT AI **//
 
@@ -65,11 +74,62 @@
 // AI values healing a little less
 #define RAI_PROMOTION_TWEAKS
 
+// AI won't make archaeologists with the purpose of sending them to sites outside their borders
+// If they don't have a sufficient number of artifact slots
+// Exception made for city state sites since you can turn them into landmarks for influence
+#define RAI_ARCHAELOGIST_OUTSIDE_BORDERS_NEED_ARTIFACT_SLOTS
+
+// Great artists now check to see if existing art slots would be used up by current archaeologists
+// and if so, starts a golden age instead of making art
+#define RAI_GREAT_ARTISTS_SAVE_SLOTS_FOR_ARCHAEOLOGISTS
+
+// AI can now use great artists to extend a golden age
+#define RAI_GREAT_ARTISTS_CAN_EXTEND_GOLDEN_AGES
+
+// AI are much less likely to send missionaries to cities without open borders
+#define RAI_RELIGION_SCORE_CITY_PENALTY_IF_NO_OPEN_BORDERS (3)
+
+//** MILITARY AI **//
+
+// AI will build more naval units if currently at war with another continent
+#define RAI_NAVY_FOCUS_IF_WARRING_ANOTHER_CONTINENT (50)
+
+// AI will build road towards a city they are trying to capture
+#define RAI_AI_BUILDS_ROAD_TOWARD_WAR_TARGET
+
+// Distance to target is squared for war target evaluations
+#define RAI_MILITARY_TARGET_WEIGHT_DISTANCE_SQUARED
+
+// Tweaks to economic value of a war target
+#define RAI_MILITARY_TARGET_WEIGHT_ECONOMIC_TWEAKS
+
+// AI will be able to use smaller attack forces to attack enemy cities in ancient and classical eras
+#define RAI_USE_BASIC_CITY_ATTACK_FORCE_IN_CLASSICAL_ERA
+
+// AI won't keep an abnormally high number of units in reserve on higher difficulties
+#define RAI_LESS_MILITARY_RESERVES_FOR_HIGHER_DIFFICULTY
+
+// AI won't convert military units to scouts if he's at war
+#define RAI_DONT_CONVERT_MILITARY_TO_SCOUTS_IF_AT_WAR
+
+// AI won't consider scouting units when calculating dominance of a zone
+#define RAI_DONT_COUNT_SCOUTS_WHEN_CALCULATING_DOMINANCE
+
+//** ECONOMIC AI **//
+
+// AI won't overly spend gold on bad land just because he's super rich
+#define RAI_DONT_WASTE_GOLD_ON_BAD_TILES
+
+
 //** BUX FIXES **//
 
-// Units now properly update the AI's map on death;
-// This means AIs no longer act as though they are being ZOC'd by a unit they just killed
-#define RAI_DEAD_UNIT_UPDATES_MAP
+// AIs no longer act as though they are being ZOC'd by a unit they just killed
+#define RAI_DEAD_UNITS_DONT_ZOC
+
+// Religious city states no longer mistakenly offer technology contests about 20x as much as intended
+#define RAI_RELIGIOUS_TECH_CONTEST_FIX
+
+// Danger values from cities are no longer 1/100 the intended value
+#define RAI_CITY_DANGER_FIX
 
 #endif
-
